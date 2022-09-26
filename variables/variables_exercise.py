7c2d3a88f057d601a1216c17390f5bccfec8e617
Hemachandar Pamanji <37032280+hemu06519@users.noreply.github.com>
2022-09-26 20:13:33 +0100

    Update variables_exercise.py

--- variables/variables_exercise.py
+++ variables/variables_exercise.py
@@ -1,9 +1,23 @@
 # Create a Variable
 
+#---------------------------------------------------------------------------------------------------------------------------
 # In python a variable is created at the time a value is assigned to it. We dont have to declare it before initialisation.
+#---------------------------------------------------------------------------------------------------------------------------
+
 a = 1
 print("value of a is ",a)
+
+#---------------------------------------------------------------------------------------------------------------------------
+# Python variables can be set to any type of value even after its first initialisation.
+#---------------------------------------------------------------------------------------------------------------------------
+
+a=5       # an integer value is assigned to a
+a="Five"  # a string value is assigned to a
+print("Value of a is ", a)
+
+#---------------------------------------------------------------------------------------------------------------------------
 # Varibales are case-sensitive. i.e.. variable a is different than variable A
+#---------------------------------------------------------------------------------------------------------------------------
 
 a = 5
 A = 10
@@ -11,3 +25,13 @@ print("value of a is ", a)
 print("value of A is ", A)
 
 #in the above example you can see a=5 is not ovveridden by A=10. Both are created and initialised as two different variables
+
+#---------------------------------------------------------------------------------------------------------------------------
+# You can find which type of the value is stored in a variable using type() build in function (BIF)
+#---------------------------------------------------------------------------------------------------------------------------
+
+a = 5
+b = "Five"
+print("Type of a is ", type(a))
+print("Type of b is ", type(b))
+
