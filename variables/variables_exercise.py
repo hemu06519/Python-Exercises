6341790b1e59175fa9beec6d84db2eb6c88f2eb5
Hemachandar Pamanji <37032280+hemu06519@users.noreply.github.com>
2022-09-26 20:06:19 +0100

    Update variables_exercise.py

--- variables/variables_exercise.py
+++ variables/variables_exercise.py
@@ -2,9 +2,9 @@
 
 # Varibales are case-sensitive. i.e.. variable a is different than variable A
 
-a=5
-A=10
-print("value of a:",a)
-print("value of A:",A)
+a = 5
+A = 10
+print("value of a:", a)
+print("value of A:", A)
 
 #in the above example you can see a=5 is not ovveridden by A=10. Both are created and initialised as two different variables
