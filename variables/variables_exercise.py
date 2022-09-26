3c5a6324c6013457aa74ffb61b6547c19f471f63
Hemachandar Pamanji <37032280+hemu06519@users.noreply.github.com>
2022-09-26 20:08:09 +0100

    Update variables_exercise.py

--- variables/variables_exercise.py
+++ variables/variables_exercise.py
@@ -1,10 +1,13 @@
 # Create a Variable
 
+# In python a variable is created at the time a value is assigned to it. We dont have to declare it before initialisation.
+a = 1
+print("value of a is ",a)
 # Varibales are case-sensitive. i.e.. variable a is different than variable A
 
 a = 5
 A = 10
-print("value of a:", a)
-print("value of A:", A)
+print("value of a is ", a)
+print("value of A is ", A)
 
 #in the above example you can see a=5 is not ovveridden by A=10. Both are created and initialised as two different variables
